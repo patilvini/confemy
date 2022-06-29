179faded8e74eba31bf2a32e9bb7c2291888e2df
Pranit <pranit7cr7@gmail.com>
2022-06-29 19:42:42 +0530

    testing editor

--- src/components/conference/DraftTesting.js
+++ src/components/conference/DraftTesting.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import { convertFromRaw, convertToRaw, convertFromHTML, EditorState } from "draft-js";
+import { convertToHTML } from "draft-convert";
+import {convertFromRaw, convertToRaw, EditorState, Editor as Edit, convertFromHTML } from "draft-js";
+import DOMPurify from "dompurify";
 
 import "./conferDetails1.scss";
 
@@ -11,11 +13,19 @@ export default function DraftTesting() {
   const [text, setText] = useState([]);
   const [data, setData] = useState([]);
   const [content, setContent] = useState("")
+  const  [convertedContent, setConvertedContent] = useState(null);
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
 
   useEffect(()=>{
-    api.get('/conferences/draft/62ad9c0c087a452f7bad9437')
+    api.get('/conferences/draft/62bc4df09ba8326f28fe64ce')
     .then((r)=>{
-        console.log(r);
+      console.log(r.data.data)
+        setContent(r.data.data)
+        let x = convertFromRaw( r.data.data.descriptions[0] )
+       console.log(x)
+       setData(convertToHTML(x))
         
     }).catch((err)=>{
         console.log(err);
@@ -23,15 +33,54 @@ export default function DraftTesting() {
 
   },[])
 
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  
+
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+
+    convertContentToHTML();
+  }
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML({
+      styleToHTML: (style) => {
+        if (style === 'BOLD') {
+          return <span style={{color: 'blue'}} />;
+        }
+      },
+      blockToHTML: (block) => {
+        if (block.type === 'PARAGRAPH') {
+          return <p />;
+        }
+      },
+      entityToHTML: (entity, originalText) => {
+        if (entity.type === 'LINK') {
+          return <a href={entity.data.url}>{originalText}</a>;
+        }
+        else if (entity.type === "IMAGE")
+        return originalText;
+      }
+    })(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+    // console.log(typeof convertedContent)
+  }
+
+  const createMarkup = (html) => {
+    return  {
+      __html: DOMPurify.sanitize(html)
+    }
+  }
+
+
 
   
 
   const submit = async ()=> {
 
+    console.log(text)
     let contents = text
+  
+    console.log(convertedContent)
+    
     try{
        const res = await api.post('/conferences/draft', {contents})
        console.log(res)
@@ -46,27 +95,57 @@ export default function DraftTesting() {
       <div className="editor">
         <div style={{ padding: "2px", minHeight: "400px", width: "100%" }}>
           <Editor
-            // readOnly={readOn}
+            // readOnly={true}
             // placeholder={placeholder}
             editorState={editorState}
-            onEditorStateChange={setEditorState}
+            onEditorStateChange = {(editorState) => {
+              handleEditorChange(editorState)
+              const contentState = editorState.getCurrentContent()
+              setText(convertToRaw(contentState))
+              console.log('content State', convertToRaw(contentState))
+      
+              setEditorState(editorState)
+              
+
+
+            }}
+           
+            
             toolbarClassName="toolbarClassName"
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
-            onChange={(e) => {setText(e.blocks)
-            
-            }}
+            // onChange={(e) => {
+              
+            //   setText(editorState.getCurrentContent())
+            //   let currentC = setText(convertToRaw(text))
+              
+           
+            // }}
           />
         </div>
       </div>
       <button onClick={submit} className="button button-green">Submit</button>
 
 
-      <div>
-        {content}
+      <div dangerouslySetInnerHTML={createMarkup(data)}></div>
+
+{/* 
+      <Edit
+         
+            // editorState={editorState}
+            onChange = {(e)=>console.log(e)}
+            readOnly={true}
+            
+           
+         
+         
+          /> */}
+        
+
+
 
         
-      </div>
+      
     </>
   );
 }
